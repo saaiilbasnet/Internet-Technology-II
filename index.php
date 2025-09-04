3fe572ecc74cb4964f498b31137addba74c0874c
Saaiil Basnet <basnetssahil@gmail.com>
2025-09-04 07:29:18 +0545

    init

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Dynamic Content Below!</h1>
+    <?php
+    $name = "Saaiil";
+echo "Hello World!";
+print "<p>Hi, I am $name.</p>";
+print " Today's Date :".date('Y-m-d');
+
+$num1 = 5;
+$num2 = 5;
+
+$sum = $num1 + $num2;
+
+echo "$sum";
+
+?>
+</body>
+</html>
---
+++ operators.php
@@ -0,0 +1,63 @@
+<?php
+
+$quantity = 30;  //Buying 30 weights
+$price = 119.95;
+$taxrate = .05;  //5% sales taxes
+
+//calculate the total 
+
+$total = $quantity * $price;
+$total = $total + ($total * $taxrate);
+
+echo "<p> Total : ".$total."</p>";
+
+//increment counter
+
+$counter = 10;
+echo "<p>Inital Counter: ".$counter."</p>";
+
+//increased by 1
+
+$counter++;
+echo "<p>Final Counter: ".$counter."</p>";
+
+// Assignment Operators
+
+$message = "Hello";
+$message .= ",World!"; //concate and assign
+
+echo "<p>".$message."</p>";
+
+$score = 100;
+$score += 50;
+
+echo "<p> Score : ".$score."</p>";
+
+// Logical Operators
+
+$age = 35;
+$isStudent = true;
+
+if($age > 18 && $isStudent){
+    echo "<p>Eligible for student discount.</p>";
+    return;
+}
+
+$rolledSix = false;
+$rolledFive = true;
+
+if($rolledSix || $rolledFive){
+    echo "<p>You win! (rolled a 5 or 6).</p>";
+}
+
+$isEmpty = false;
+
+if(!$isEmpty){
+    echo "<p>The variable is not empty.</p>";
+}
+
+$username = $_GET['user'] ?? 'Guest';
+
+echo "<p>Welcome, ".$username ."!</p>";
+
+?>
